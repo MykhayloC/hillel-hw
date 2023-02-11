7cf65e1e3cd9976664e87fd11a6c98e2504de1cf
MykhayloC <chugayev_misha@ukr.net>
2023-02-11 14:05:47 +0200

    hw-5 has finished

--- hw-5/script.js
+++ hw-5/script.js
@@ -1,2 +1,9 @@
 'use strict';
 
+const firstNum = +prompt('Введіть перше число:');
+const secondNum = +prompt('Введіть друге число:');
+const thirdNum = +prompt('Введіть третє число:');
+
+const  arithmeticMean = (firstNum + secondNum + thirdNum) / 3;
+
+alert(`Середнє арифметичне значення введених чисел складає: ${arithmeticMean}`);
