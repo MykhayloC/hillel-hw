1a53ca4565cc60f37522c47372b356f1d493755c
Mykhaylo Chuhayev <chugayev_misha@ukr.net>
2023-03-08 18:09:23 +0200

    code was simplyfied by cancel of array data

--- hw-15/script.js
+++ hw-15/script.js
@@ -3,7 +3,6 @@
 const h2 = document.createElement('h2');
 const div = document.createElement('div');
 const table = document.createElement('table');
-const data = [...Array(100).keys()].map(i => ++i);
 
 h2.setAttribute('style', 'text-align: center')
 h2.innerHTML = 'Домашнє завдання 15';
@@ -18,25 +17,21 @@ table.style.cssText = `
     width: 500px;
     margin: auto;
 `;
-// table.setAttribute('style', 'border: 1px solid #ccc');
 
-for (let i = 0; i < 10; i++) {
-    let tableRow = document.createElement('tr');
-    let n = 0;
-    for (let j = 1; j <= 10; j++) {
-        let tableCell = document.createElement('td');
-        tableCell.style.cssText = `
+const cellStyle = `
             border: 1px solid #ccc;
             padding: 10px;
             text-align: center;
         `;
+
+for (let i = 0; i < 10; i++) {
+    let tableRow = document.createElement('tr');
+
+    for (let j = 1; j <= 10; j++) {
+        let tableCell = document.createElement('td');
+        tableCell.innerHTML = i * 10 + j;
+        tableCell.style.cssText = cellStyle;
         tableRow.append(tableCell);
     }
-    n += 10;
     table.append(tableRow);
-}
-
-let cells = document.getElementsByTagName('td');
-for (let i = 0; i < 100; i++) {
-    cells[i].innerHTML = data[i];
 }
