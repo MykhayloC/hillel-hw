866783a341bd1dcf1ea844b796f8587ea7236326
Mykhaylo Chuhayev <chugayev_misha@ukr.net>
2023-04-10 16:09:28 +0300

    hw done

---
+++ hw-23_fetch/data.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const cities = [
+    'Kyiv',
+    'Sumy',
+    'Chernihiv',
+    'Cherkasy',
+    'Poltava',
+    'Kharkiv',
+    'Donetsk',
+    'Luhansk',
+    'Zaporizhia',
+    'Dnipro',
+    'Mykolayiv',
+    'Kherson',
+    'Odesa',
+    'Kropyvnytskyi',
+    'Zhytomyr',
+    'Vinnytsia',
+    'Rivne',
+    'Lutsk',
+    'Ternopil',
+    'Chernivtsi',
+    'Ivano-Frankivsk',
+    'Uzhhorod',
+    'Lviv',
+];
+
+export {cities};
